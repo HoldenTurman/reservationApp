184e83ba52c85e91fb6e5830dfaf28cb8cbf2dc9
Holden Turman <holden.turman1@gmail.com>
2025-11-20 14:15:40 -0700

    Refactor app.js for better middleware and routes
    Refactor app.js to improve structure and organization.

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,14 +1,30 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
+const app = express();
 
-// Serve static frontend files
-const frontendPath = path.join(__dirname, "../../front-end/build"); 
+// --------------------
+// Middleware
+// --------------------
+app.use(express.json());
+
+// --------------------
+// API Routes
+// --------------------
+// Example placeholder:
+// const reservationsRouter = require("./routes/reservations");
+// app.use("/api/reservations", reservationsRouter);
+
+// --------------------
+// Serve Frontend
+// --------------------
+const frontendPath = path.join(__dirname, "../../front-end/build");
 app.use(express.static(frontendPath));
 
-// Catch-all route to serve index.html for React Router
+// Catch-all route to support React Router paths
 app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
+// Export app for server.js
 module.exports = app;
 
