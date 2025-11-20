186ba16040030e433ab694d9933c9f97b804d6b8
Holden Turman <holden.turman1@gmail.com>
2025-11-20 14:21:14 -0700

    Refactor comments for clarity in app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -2,29 +2,26 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// --------------------
-// Middleware
-// --------------------
+// Middleware for parsing JSON
 app.use(express.json());
 
 // --------------------
-// API Routes
+// API routes placeholder
 // --------------------
-// Example placeholder:
 // const reservationsRouter = require("./routes/reservations");
 // app.use("/api/reservations", reservationsRouter);
 
 // --------------------
-// Serve Frontend
+// Serve frontend
 // --------------------
 const frontendPath = path.join(__dirname, "../../front-end/build");
 app.use(express.static(frontendPath));
 
-// Catch-all route to support React Router paths
+// Catch-all route for React Router
 app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-// Export app for server.js
 module.exports = app;
+
 
