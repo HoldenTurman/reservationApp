cd4bc3580e8aba2f48dbc11955c864e012fe8ada
Holden Turman <holden.turman1@gmail.com>
2025-11-20 14:12:48 -0700

    Update app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,24 +1,14 @@
 const path = require("path");
-
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
-
 const express = require("express");
-const cors = require("cors");
 
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
-const reservationsRouter = require("./reservations/reservations.router");
-const tablesRouter = require("./tables/tables.router");
+// Serve static frontend files
+const frontendPath = path.join(__dirname, "../../front-end/build"); 
+app.use(express.static(frontendPath));
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use("/reservations", reservationsRouter);
-app.use("/tables", tablesRouter);
-
-app.use(notFound);
-app.use(errorHandler);
+// Catch-all route to serve index.html for React Router
+app.get("*", (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 module.exports = app;
+
