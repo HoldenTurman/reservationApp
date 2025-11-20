ec3d072909011e2fc22498cc02529f6d466edad7
Holden Turman <holden.turman1@gmail.com>
2025-11-20 15:09:15 -0700

    Update server.js

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,17 +1,29 @@
 const { PORT = 5001 } = process.env;
-const app = require("./app"); // <-- no ./src
+const app = require("./app");
 const knex = require("./db/connection");
 
-knex.migrate
-  .latest()
-  .then(() => {
+async function startServer() {
+  try {
+    // Run migrations on startup
+    console.log("Running migrations...");
+    await knex.migrate.latest();
+
+    // Run seeds (optional, remove if you don't want to seed every startup)
+    console.log("Running seeds...");
+    await knex.seed.run();
+
+    // Start the server
     app.listen(PORT, () => {
-      console.log(`Listening on port ${PORT}`);
+      console.log(`Server is listening on port ${PORT}!`);
     });
-  })
-  .catch((error) => {
-    console.error(error);
-    knex.destroy();
-  });
+  } catch (error) {
+    console.error("Error starting server:", error);
+    await knex.destroy();
+    process.exit(1);
+  }
+}
+
+startServer();
+
 
 
